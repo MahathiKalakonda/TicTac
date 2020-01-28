79483ed4cff26e929c2b1f9e83ee55ed55537332
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-01-28 14:57:07 +0530

    Add tictac.file to repo

---
+++ tictac.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+import java.lang.*;
+class tictac
+{	static int board[][]=new int[3][3];
+	public static boolean test(int row,int column,int symbol)
+	{
+		if(board[(row-1+3)%3][column]==symbol && board[(row-2+3)%3][column]==symbol)
+			return true;
+		if(board[row][(column-1+3)%3]==symbol && board[row][(column-2+3)%3]==symbol)
+			return true;
+		if(row==column)
+            if(board[(row-1+3)%3][(column-1+3)%3]==symbol && board[(row-2+3)%3][(column-2+3)%3]==symbol)
+        		return true;
+		if((row==1 && column ==1)||(Math.abs(row-column)==2))
+			if(board[(row+1+3)%3][(column-1+3)%3]==symbol && board[(row+2+3)%3][(column-2+3)%3]==symbol)
+				return true;
+		return false;
+	}
+	public static void main(String args[])
+	{
+		Scanner in=new Scanner(System.in);
+		int filled=0;
+		boolean Status=false;
+		while(filled<9)
+		{	
+			System.out.println("Enter position of player"+((filled%2)+1));
+			System.out.println("Row : ");
+			int row=in.nextInt();
+			System.out.println("Column : ");
+			int col=in.nextInt();
+			if(board[row-1][col-1]!=0)
+			{
+				System.out.println("Position is already filled, Please enter another position ");
+			}
+			else
+			{
+				board[row-1][col-1]=(filled%2)+1;
+				Status=test(row-1,col-1,(filled%2)+1);
+				if(Status)
+				{
+					System.out.println("Player"+((filled%2)+1)+" won the game");
+					break;
+				}
+				filled++;
+			}
+		}
+		if(filled==9)
+			System.out.println("Draw Match");
+	}	
+}
