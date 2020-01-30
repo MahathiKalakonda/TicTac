e9341c918e3c53b816e59f2feb0034ea7887840e
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-01-30 14:10:28 +0530

    Modification to Tester method in Board class

--- Board.java
+++ Board.java
@@ -1,9 +1,10 @@
+import java.util.Set;
+import java.util.HashSet;
 class Board
 {
     char board[]=new char[9];
     int turns_remaining=9;
-    int conditions[][]=new int[8][3];
-    conditions={{0,1,2},{0,3,6},{0,4,8},{3,4,5},{6,7,8},{1,4,7},{2,5,8},{2,4,6}};
+    int conditions[][]={{0,1,2},{0,3,6},{0,4,8},{3,4,5},{6,7,8},{1,4,7},{2,5,8},{2,4,6}};
     public boolean validate_entry(int position,char symbol)
     {
         if(board[position]!='\0')
@@ -27,10 +28,13 @@ class Board
     public boolean Tester(int position)
     {
         int number_conditions=8;
-        char 
         for(int condition=0;condition<number_conditions;condition++)
         {
-
+            char[] symbols_in_condition=new char[3];
+            for(int position_in_condition=0,symbol_index=0;position_in_condition<3;position_in_condition++,symbol_index++)
+                symbols_in_condition[symbol_index]=board[conditions[condition][position_in_condition]];
+            Set<Character> symbol_set = new HashSet<Character>();
+            symbol_set.addAll(symbols_in_condition);
         }
         return false;
     }
